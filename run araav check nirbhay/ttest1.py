e3076e5ac18eef44c00056aa9feee3ad90176546
subscribetoncmystery <140418907+subscribetoncmystery@users.noreply.github.com>
2023-08-02 21:24:09 +0530

    Add files via upload

---
+++ run araav check nirbhay/ttest1.py
@@ -0,0 +1,11 @@
+a = 33
+b = "33"
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+def my_function(fname):
+    print(fname + "refsnes")
+my_function ("emil")   
+my_function ("tobias")
+my_function ("Linus")  
